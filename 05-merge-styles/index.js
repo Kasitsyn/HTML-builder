499e84ba055273c5e41d60ec11cc9a60959877f4
kasitsyn <kasitsyn@gmail.com>
2022-05-23 23:01:51 +0700

    feat: make 5th task

---
+++ 05-merge-styles/index.js
@@ -0,0 +1,28 @@
+const fs = require('fs');
+const path = require('path');
+
+fs.readdir(path.join(__dirname, 'styles'), { withFileTypes: true }, (err, files) => {
+   
+  if (err)
+    console.log(err);
+  else {
+    
+    for (let file of files) {
+      if (file.isFile() && path.extname(file.name) === '.css'){ 
+        const rs = fs.createReadStream(path.join(__dirname, 'styles', file.name), 'utf-8');
+        rs.on('data', chunk => {
+          fs.appendFile(
+            path.join(__dirname, 'project-dist', 'bundle.css'),
+            `${chunk}\n`,
+            (err) => {
+              if (err) throw err;
+               
+            }
+          );
+        });
+        
+      }
+    }
+
+  }
+});
