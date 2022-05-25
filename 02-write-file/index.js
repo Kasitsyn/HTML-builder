d60d19c4ff7aefa00ccce7f35f09fb1c1e360ec1
kasitsyn <kasitsyn@gmail.com>
2022-05-25 22:44:46 +0700

    feat: add message after exit

--- 02-write-file/index.js
+++ 02-write-file/index.js
@@ -1,9 +1,13 @@
 const fs = require('fs');
 const path = require('path');
 const readline = require('readline');
-const { stdin: input, stdout: output } = require('process');
+const { stdin: input, stdout: output, exit } = require('process');
+
+
 
 const rl = readline.createInterface({ input, output });
+
+
 
 fs.writeFile(
   path.join(__dirname, 'count.txt'),
@@ -15,10 +19,15 @@ fs.writeFile(
 );
 
 rl.on('line', input => {
+
+  
+
   if (input.toLowerCase() === 'exit') {
+    console.log('Thank you, bye');
     rl.close();
     return;
   }
+
   fs.appendFile(
     path.join(__dirname, 'count.txt'),
     `${input}\n`,
@@ -28,4 +37,11 @@ rl.on('line', input => {
     }
   );
 });
+
+process.on('exit', () => {
+  console.log('Thank you, bye');
+  exit();
+});
+
+
 
