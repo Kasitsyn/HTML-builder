fe300919310ed8c9031694c05dd1287de324b68c
kasitsyn <kasitsyn@gmail.com>
2022-05-25 23:11:01 +0700

    fix: make bundle rewritable

--- 05-merge-styles/index.js
+++ 05-merge-styles/index.js
@@ -2,25 +2,31 @@ const fs = require('fs');
 const path = require('path');
 
 fs.readdir(path.join(__dirname, 'styles'), { withFileTypes: true }, (err, files) => {
-   
+
   if (err)
     console.log(err);
   else {
-    
+
     for (let file of files) {
-      if (file.isFile() && path.extname(file.name) === '.css'){ 
+      if (file.isFile() && path.extname(file.name) === '.css') {
         const rs = fs.createReadStream(path.join(__dirname, 'styles', file.name), 'utf-8');
         rs.on('data', chunk => {
+          fs.writeFile(path.join(__dirname, 'project-dist', 'bundle.css'),
+            '',
+            (err) => {
+              if (err) throw err;
+
+            });
           fs.appendFile(
             path.join(__dirname, 'project-dist', 'bundle.css'),
             `${chunk}\n`,
             (err) => {
               if (err) throw err;
-               
+
             }
           );
         });
-        
+
       }
     }
 
